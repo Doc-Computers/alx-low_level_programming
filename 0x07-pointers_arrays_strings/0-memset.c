01acc2aeec44597261423691101c92519ba9a3cd
Doc-Computers <husseinchiaji@gmail.com>
2022-09-27 03:30:19 -0700

    1. memcpy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,15 +6,16 @@
  * @b: char to be used
  * @n: number of bytes to be used
  *
- * return pointer to the memory block
+ * Return: pointer to the memory block
  */
 
 char *_memset(char *s, char b, unsigned int n)
+
 {
 	while (n)
 	{
 		s[n - 1] = b;
 		n--;
 	}
-	return (s)
+	return (s);
 }
